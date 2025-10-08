91b6ba06aa6a2d00125d3033ad2860d99dafbe34
Dominik Sedusov <dom1k11@outlook.com>
2025-10-08 13:20:16 +0300

    websocket simple implementation

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,5 +1,36 @@
+import { useState, useEffect } from "react";
+import { socket } from "../socket/connection";
+
 const MainPage = () => {
-  return <h1>Hello world </h1>;
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    socket.on("text-update", (newText: string) => {
+      setText(newText);
+    });
+
+    return () => {
+      socket.off("text-update");
+    };
+  }, []);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setText(value);
+    socket.emit("text-change", value);
+  };
+
+  return (
+    <div>
+      <h1>Collaborative input test</h1>
+      <input
+        type="text"
+        value={text}
+        onChange={handleChange}
+        placeholder="Type here..."
+      />
+    </div>
+  );
 };
 
 export default MainPage;
